f6cc8d7a337bfb0dd2bdf8aa774ebd674d968cdd
phamthao27 <trungthao020797@gmail.com>
2021-01-21 23:25:05 +0700

    done2

--- resources/views/Component/lien-he.blade.php
+++ resources/views/Component/lien-he.blade.php
@@ -7,43 +7,43 @@
 
 
     <div class="row justify-content-center">
-      <div class="col-lg-8">
+    <div class="col-lg-8">
         <h4>Gửi lời nhắn cho chúng tôi</h4>
         <p>Đừng ngần ngại chúng tôi sẽ trả lời một cách sớm nhất và nhanh nhất!!</p>
         <ul class="contact-items">
-          <li class="contact-item"><address>Quận 12, thành phố Hồ Chí Minh</address></li>
-          <li class="contact-item"><a href="tel:+1-800-1554-456-123">0967979869</a></li>
-          <li class="contact-item"><a href="mailto:themesupport@gmail.com">suport@gmail.com</a></li>
+        <li class="contact-item"><address>Quận 12, thành phố Hồ Chí Minh</address></li>
+        <li class="contact-item"><a href="tel:+1-800-1554-456-123">0967979869</a></li>
+        <li class="contact-item"><a href="mailto:themesupport@gmail.com">suport@gmail.com</a></li>
         </ul>
 
         <!-- Contact Form -->
         <form id="contact-form" class="contact-form mt-30 mb-30" method="post" action="#">
-          <div class="contact-name">
+        <div class="contact-name">
             <label for="name">Họ và tên:<abbr title="required" class="required">*</abbr></label>
             <input name="name" id="name" type="text">
-          </div>
-          <div class="contact-email">
+        </div>
+        <div class="contact-email">
             <label for="email">Email: <abbr title="required" class="required">*</abbr></label>
             <input name="email" id="email" type="email">
-          </div>
-          <div class="contact-subject">
+        </div>
+        <div class="contact-subject">
             <label for="email">Điện thoại:</label>
             <input name="subject" id="subject" type="tel">
-          </div>
-          <div class="contact-subject">
+        </div>
+        <div class="contact-subject">
             <label for="email">Tiêu đề:</label>
             <input name="subject" id="subject" type="text">
-          </div>
-          <div class="contact-message">
+        </div>
+        <div class="contact-message">
             <label for="message">Lời nhắn: <abbr title="required" class="required">*</abbr></label>
             <textarea id="message" name="message" rows="7" required="required"></textarea>
-          </div>
+        </div>
 
-          <input type="submit" class="btn btn-lg btn-color btn-button" value="Send Message" id="submit-message">
-          <div id="msg" class="message"></div>
+        <input type="submit" class="btn btn-lg btn-color btn-button" value="Send Message" id="submit-message">
+        <div id="msg" class="message"></div>
         </form>
 
-      </div>
     </div>
-  </div>
+    </div>
+</div>
 @endsection
